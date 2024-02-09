f4494ce012c5cbb4d3d51eed325cb24e975f4140
Francesco Cerbai <1ecerbaifrancesco@gmail.com>
2024-02-09 15:38:19 +0100

    main ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,1 +1,20 @@
 #include "RPN.hpp"
+
+int main(int argc, char **argv) 
+{
+    try
+    {
+        if (argc == 2)
+        {
+            RPN::parse(argv[1]);
+            RPN::print();
+        }
+        else
+            throw std::invalid_argument("Error! Wrong number of arguments.");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return 0;
+}
